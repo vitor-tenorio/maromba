e1fc3c91e61fea6a8dc22c6425946dfa9799b601
vitor <vitor.tenorio@compjunior.com.br>
2023-02-12 23:56:04 -0300

    Muda cor do banco

--- banco.c
+++ banco.c
@@ -2,7 +2,7 @@ void montaBanco()
 {
     glTranslatef(0.5f, 0.0f, 0.0f);
     glBegin(GL_QUADS);
-        glColor3f(0.0, 0.0, 1.0);  // Define a cor azul
+        glColor3f(0.4, 0.4, 0.8);  // Define a cor azul
         // Face frontal
         glVertex3f(-1.0, -0.5, -0.5);
         glVertex3f(1.0, -0.5, -0.5);
@@ -15,6 +15,7 @@ void montaBanco()
         glVertex3f(1.0, -0.6, -1.0);
         glVertex3f(-1.0, -0.6, -1.0);
 
+        glColor3f(0.0, 0.0, 1.0);  // Define a cor azul
         // Face superior
         glVertex3f(-1.0, -0.5, -1.0);
         glVertex3f(1.0, -0.5, -1.0);
